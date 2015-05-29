65ee82ee90bba1892fd191f1bbd48a66de122081
joss <jfrodsla@gmail.com>
2015-05-29 10:57:09 -0700

    add webpage functionality for user input; form is working as expected; capilization is as expected

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,23 @@ var replacer = function(userSentence, oldWord, newWord) {
   var fixedSentence = sentence.replace(userOldWord, newWord);
   return fixedSentence;
 };
+
+
+
+$(document).ready(function(){
+
+  $("form#string-words").submit(function(event){
+      var userSentence = $("input#sentence").val();
+      var oldWord = $("input#replace").val();
+      var newWord = $("input#new-word").val();
+      var result = replacer(userSentence, oldWord, newWord);
+
+      $(".new-sentence").text(result);
+      $("#result").show();
+
+
+    event.preventDefault();
+  });
+
+
+});
